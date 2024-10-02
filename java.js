8a00d292d1c599b823290b9d27e3efcc95635622
jesusin567 <162446685+jesusin567@users.noreply.github.com>
2024-10-02 10:40:27 -0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,25 @@
+function muestraPintor() {
+
+   var infoContacto = ["Diego", "Rivera", "Guanajuato", "México", 812345678];
+
+   console.log(infoContacto);
+
+}
+function muestraCompras() {
+
+   var listaSupermercado = ["arroz", "frijoles", "lentejas", "pan"];
+
+   var listaOficina = ["clips", "papel", "lapices"];
+
+   listaOficina.push("Post-its");
+
+   console.log(listaOficina);
+
+} 
+var biblioteca = ["Cien Años de Soledad", "Pedro Páramo", "La Casa de los Espíritus"];
+
+biblioteca.push("El Laberinto de la Soledad");
+
+biblioteca[1] = "El Llano en Llamas";
+
+console.log(biblioteca);
